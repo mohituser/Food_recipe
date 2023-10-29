460044d0050a8b9b37c591b58700e22504327b67
mohituser <122739741+mohituser@users.noreply.github.com>
2023-10-30 00:12:26 +0530

    Update schema.js

--- models/schema.js
+++ models/schema.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-// const validator=require("validator");
+const validator=require("validator");
 const bcrypt=require("bcryptjs");
 const jwt=require("jsonwebtoken");
 const crypto=require("crypto");//dafault package of nodejs to genrate random string
